4f80f76b3936a985e12a2612e40a8f7e3781f989
Hans Erik Jepsen <hanserikjepsen@hotmail.com>
2024-03-22 13:54:39 +0000

    Moved application bootstrapping into

--- src/App.php
+++ src/App.php
@@ -2,7 +2,12 @@
 
 namespace Vortrixs\Portfolio;
 
+use Psr\Http\Message\StreamFactoryInterface;
 use Slim\App;
+use Slim\Psr7\Factory\StreamFactory;
+use Vortrixs\Portfolio\SharedKernel\UrlHelper;
+use Vortrixs\Portfolio\Home\Controller as HomeController;
+use Vortrixs\Portfolio\Portfolio\Controller as PortfolioController;
 
 function createApp(): App
 {
@@ -10,7 +15,12 @@ function createApp(): App
 
     $app = \DI\Bridge\Slim\Bridge::create($container);
 
-    $container->make(Provider::class);
+    $container->set(StreamFactoryInterface::class, $container->get(StreamFactory::class));
+
+    $container->call(function (UrlHelper $urlHelper) {
+        $this->router->get($urlHelper->home, HomeController::class);
+        $this->router->get($urlHelper->portfolio, PortfolioController::class);
+    });
 
     $app->addErrorMiddleware(true, false, false);
 
