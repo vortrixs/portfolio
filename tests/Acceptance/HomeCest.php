2168eea099324c15a5b3a6971f00dfd1187ff596
Hans Erik Jepsen <hanserikjepsen@hotmail.com>
2025-04-15 11:45:26 +0000

    Cleaned up acceptance test

--- tests/Acceptance/HomeCest.php
+++ tests/Acceptance/HomeCest.php
@@ -22,12 +22,10 @@ class HomeCest
         $model = $I->get(Model::class);
 
         $entities = [
-            new Entity(1, 'Junior Backend Developer', 'Company #1', 'Full-time','2010 - 2011', ['PHP', 'MySQL'], 'This was my first job'),
-            new Entity(2, 'Fullstack Developer', 'Company #2', 'Contract','2012 - 2014', ['PHP', 'Postgres', 'TypeScript', 'Svelte'], 'This was an awesome job')
+            new Entity(1, 'Junior Backend Developer', 'Company #1', 'Full-time', '2010 - 2011', ['PHP', 'MySQL'], 'This was my first job'),
+            new Entity(2, 'Fullstack Developer', 'Company #2', 'Contract', '2012 - 2014', ['PHP', 'Postgres', 'TypeScript', 'Svelte'], 'This was an awesome job')
         ];
         array_walk($entities, $model->create(...));
-
-        $viewModel = new ViewModel($model);
 
         $I->amOnPage($urlHelper->frontpage);
 
@@ -35,16 +33,15 @@ class HomeCest
 
         $I->seeElement('main');
         $I->see('CV', 'h1');
+        
+        $I->see('Junior Backend Developer @ Company #1', '.card > .card__title');
+        $I->see('Full-time 2010 - 2011', '.card > .card__line');
+        $I->see('PHP, MySQL', '.card > .card__line');
+        $I->see('This was my first job', '.card > .card__text');
 
-        $list = $viewModel->getCvList();
-
-        $I->assertCount(2, $list);
-
-        foreach ($list as $cv) {
-            $I->see("{$cv['position']} @ {$cv['company']}", '.card > .card__title');
-            $I->see("{$cv['employmentType']} {$cv['length']}", '.card > .card__line');
-            $I->see(implode(', ', $cv['tags']), '.card > .card__line');
-            $I->see($cv['description'], '.card > .card__text');
-        }
+        $I->see('Fullstack Developer @ Company #2', '.card > .card__title');
+        $I->see('Contract 2012 - 2014', '.card > .card__line');
+        $I->see('PHP, Postgres, TypeScript, Svelte', '.card > .card__line');
+        $I->see('This was an awesome job', '.card > .card__text');
     }
 }
